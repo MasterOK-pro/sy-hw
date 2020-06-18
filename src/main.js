38cca641ba17a83836c96b698aefa55dbb5dd66e
MasterOK-pro <574216830@qq.com>
2020-06-18 20:25:04 +0800

    添加vant插件。在我的页面下编辑了"顶部登录栏","优惠券","订单部分"轮播图基本完成。

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import router from './router'
 import store from './store'
 import './css/style.css'
 
+import { Icon } from 'vant'
+import 'vant/lib/index.css'
+import 'vant/lib/index.css'
+Vue.use(Icon)
+
+import { Swipe, SwipeItem } from 'vant'
+
+Vue.use(Swipe);
+Vue.use(SwipeItem);
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/MineView.vue
+++ src/views/MineView.vue
@@ -1,16 +1,7 @@
 <template>
   <div class="mine">
-    <div class="mine-topbar">
-      <div class="mine-topbar-left">
-        <img src="../../public/imgs/app-mine-user-dflt.png" alt="" class="topbar-left-user">
-        <router-link to="">登录/注册</router-link>
-      </div>
-      <div class="mine-topbar-right">
-        <div>设置</div>
-        <div>消息</div>
-      </div>
-    </div>
-    <mine-mid-body></mine-mid-body>
+    
+    <mine-body></mine-body>
     <!-- app首页底部导航按钮 -->
     <home-page-footer></home-page-footer>
   </div>
@@ -18,13 +9,13 @@
 
 <script>
 import HomePageFooter from "../components/HomePageFooter.vue";
-import MineMidBody from '../components/MineMidBody.vue'
+import MineBody from '../components/MineBody.vue'
 
 export default {
   components: {
     HomePageFooter: HomePageFooter,
     /* es6简写，组件选项对象和自定义标签名同名 */
-    MineMidBody
+    MineBody
   }
 };
 </script>
@@ -32,51 +23,9 @@ export default {
 <style scoped>
 .mine {
   height: 100%;
-  color: green;
   display: flex;
   flex-direction: column;
   position: relative;
-}
-/* 登录页顶栏 */
-.mine-topbar{
-  height: 2rem;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: rgb(253, 57, 57);
-  box-sizing: content-box;
-  position: fixed;
-  top: 0;
-}
-
-/* 顶栏左侧部分 */
-.mine-topbar-left{
-  height: 1.2rem;
-  margin-left: .9rem;
-  display: flex;
-  align-items: center;
-}
-.mine-topbar-left a{
-  font-size: 0.8rem;
-  text-decoration: none;
-  color: #fff;
-  line-height: 100%;
-}
-.mine-topbar-left img{
-  max-height: 100%;
-  margin-right: 0.5em;
-}
-
-/* 顶栏右侧部分 */
-.mine-topbar-right{
-  display: flex;
-  align-items: center;
-  margin-right: 0.3rem;
-  font-size: 0.5rem;
-  color: #fff;
-}
-.mine-topbar-right div{
-  padding-right: 0.5rem;
+  background-color: #f8f8f8;
 }
 </style>>
